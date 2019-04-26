2e91ca2e2970045a1a0ace13dd280ca536f1e776
Harlan Gomes <harlangomesnascimento@outlook.com>
2019-04-25 22:54:45 -0300

    Crud service added in module

---
+++ src/app/task-manager/task-manager.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Task } from "./task-manager.model";
+
+@Injectable()
+export class TaskManagerService{
+    constructor(private http: HttpClient) { }
+
+    public recoverAll(){
+        return this.http.get<any[]>('tasks/').toPromise();
+    }
+
+    public recoverById(id: string){
+        return this.http.get<any[]>('tasks/' + id).toPromise();
+    }
+
+    public create(task : Task){
+        return this.http.post<any>('tasks/', task ).toPromise();
+    }
+
+    public update(task : Task){
+        return this.http.put<any>('tasks/', task).toPromise();
+    }
+
+    public delete(id: number){
+        return this.http.delete<any>('tasks/'+ id).toPromise();
+    }
+}
